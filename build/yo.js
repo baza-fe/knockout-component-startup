550a9186a733d78f34eb308eb84f3ee9e744ffbe
BinRui Guan <binrui.guan@gmail.com>
2016-10-11 14:09:51 +0800

    Add yo

---
+++ build/yo.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+
+const name = process.argv[process.argv.length - 1];
+const dir = process.argv[process.argv.length - 2];
+
+if (name && dir && process.argv.length >= 4) {
+    scaffold(dir, name);
+} else {
+    console.error('$node build/yo.js demo3 "template-name-in-double-quote"');
+}
+
+function scaffold(dir, name) {
+    const content = render(name);
+
+    try {
+        fs.mkdirSync(`./src/component/${dir}/`);
+    } catch (e) {}
+
+    fs.mkdirSync(`./src/component/${dir}/${name}`);
+    fs.writeFileSync(`./src/component/${dir}/${name}/${name}.js`, content.js);
+    fs.writeFileSync(`./src/component/${dir}/${name}/${name}.scss`, content.scss);
+    fs.writeFileSync(`./src/component/${dir}/${name}/${name}.tpl`, content.tpl);
+}
+
+function renderJs(name) {
+    return [
+        `import style from './${name}.scss';`,
+        `import template from './${name}.tpl';`,
+        '',
+        `export default {`,
+        '    constructor(opts) {',
+        '    },',
+        '',
+        '    defaults: {',
+        '    },',
+        '',
+        '    methods: {',
+        '    }',
+        '}'
+    ].join('\n');
+};
+
+function renderStyle(name) {
+    return [
+        `$name: '${name}';`,
+        '',
+        '.#{$name} {',
+        '}',
+        '',
+        '// scaffloding',
+        '// ====='
+    ].join('\n');
+}
+
+function renderTpl(name) {
+    return [
+        `<div class="${name}">`,
+        '</div>'
+    ].join('\n');
+}
+
+function render(name) {
+    return {
+        js: renderJs(name),
+        scss: renderStyle(name),
+        tpl: renderTpl(name)
+    };
+}
