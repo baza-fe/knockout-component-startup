52c6f008ddd12578076b4bde26b0b341c3c073aa
BinRui Guan <binrui.guan@gmail.com>
2016-10-22 01:12:31 +0800

    [build] 0.0.3

--- src/js/entry/index.js
+++ src/js/entry/index.js
@@ -1,7 +1,7 @@
-// import '../../component/demo1/markdown-editor/markdown-editor';
+import '../../component/demo1/markdown-editor/markdown-editor';
 // import '../../component/demo2/comment-box/comment-box';
 // import '../../component/demo3/index/index';
 // import '../../component/demo4/person/person';
-import '../../component/demo5/todo/todo';
+// import '../../component/demo5/todo/todo';
 
 ko.applyBindings(null);
